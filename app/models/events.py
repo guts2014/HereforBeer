c743c9d68f906c746266286a7809637d37564ecd
Tom Wallis <tom@tom-wallis.com>
2014-10-11 14:44:45 +0100

    Changed table name in events model

--- app/models/events.py
+++ app/models/events.py
@@ -2,7 +2,7 @@ from app import db
 from sqlalchemy import Column, Integer, Text, DateTime
 class Event(db.Model):
 
-	__tablename__ = "terrorEvents"
+	__tablename__ = "event"
 	event_id = Column(db.Integer, primary_key=True)
 	year = Column(db.Integer)
 	month = Column(db.Integer)
