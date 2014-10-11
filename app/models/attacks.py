89fca6a10e32cdefa9e21d7ae9b092db58b3a4d5
Tom Wallis <tom@tom-wallis.com>
2014-10-11 10:59:41 +0100

    Renaming attacks to events

--- app/models/attacks.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from sqlalchemy import Column, Integer, Text, Datetime
-
-class Attack(db.Model):
-
-    __tablename__ = 'attacks'
-
-    event_id = Column(db.Integer, primary_key=True)
-
