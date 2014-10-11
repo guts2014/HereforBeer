c54ae1df318bdcf96373cecb9c0fd46d449b170a
Darren Burns <darrenb900@gmail.com>
2014-10-11 10:23:34 +0100

    Started sqlalchemy

--- app/models/attacks.py
+++ app/models/attacks.py
@@ -1,1 +1,9 @@
-__author__ = 'Darren'
+from app import db
+from sqlalchemy import Column, Integer, Text, Datetime
+
+class Attack(db.Model):
+
+    __tablename__ = 'attacks'
+
+    event_id = Column(db.Integer, primary_key=True)
+
